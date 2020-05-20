14bf19cb00c599f6b0cbf1527a0ae85b6584a03e
@snowman <jimmoffitt@yahoo.com>
2020-05-19 22:51:05 -0600

    Update tweets.sql

--- schema/tweets.sql
+++ schema/tweets.sql
@@ -1,1 +1,9 @@
-#CREATE TABLE sql
+#CREATE TABLE sql 
+CREATE TABLE scores(
+  team_id integer,
+  team_name VARCHAR (30),
+  hole integer,
+  score integer,
+  time_stamp TIMESTAMP,
+  over_under integerMP
+);
